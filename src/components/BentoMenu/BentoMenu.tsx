73ad64c96ab8894355a004d87a722482a9f25217
tenax66 <hiyoshir+tenax66@gmail.com>
2025-03-18 22:10:37 +0900

    change modal menu color

--- src/components/BentoMenu/BentoMenu.tsx
+++ src/components/BentoMenu/BentoMenu.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import styles from './BentoMenu.module.scss';
+import CardDouble from '../CardDouble';
 
 export const BentoMenu = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -17,22 +18,18 @@ export const BentoMenu = () => {
       {isOpen && (
         <div className={styles.overlay} onClick={() => setIsOpen(false)}>
           <div className={styles.menu} onClick={e => e.stopPropagation()}>
-            <div className={styles.menuHeader}>
-              <h2>メニュー</h2>
-              <button className={styles.closeButton} onClick={() => setIsOpen(false)}>
-                ✕
-              </button>
-            </div>
-            <div className={styles.menuContent}>
-              <ul>
-                <li>ホーム</li>
-                <li>最新の短歌</li>
-                <li>人気の短歌</li>
-                <li>設定</li>
-                <li>ヘルプ</li>
-                <li>お問い合わせ</li>
-              </ul>
-            </div>
+            <CardDouble title="メニュー">
+              <div className={styles.menuContent}>
+                <ul>
+                  <li>ホーム</li>
+                  <li>最新の短歌</li>
+                  <li>人気の短歌</li>
+                  <li>設定</li>
+                  <li>ヘルプ</li>
+                  <li>お問い合わせ</li>
+                </ul>
+              </div>
+            </CardDouble>
           </div>
         </div>
       )}
