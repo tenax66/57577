e930850eacff71cf9e405299d9ca53891c18d4d0
Ryota Hiyoshi <115792382+tenax66@users.noreply.github.com>
2025-03-19 21:57:29 +0900

    Feature/search (#18)
    * implement search
    
    * add delete sql
    
    * create indexing function
    
    * tokenizer for suffix-matches

---
+++ migrations/0005_create_tankas_fts_table.sql
@@ -0,0 +1,1 @@
+CREATE VIRTUAL TABLE IF NOT EXISTS fts USING fts5(segments);
--- src/types/types.ts
+++ src/types/types.ts
@@ -9,12 +9,12 @@ export type TankasResponse = {
 };
 
 export type Tanka = {
-    id: number;
-    content: string;
-    user_id: number;
-    created_at: string;
-    display_name: string;
-    clerk_id: string;
-    is_liked: boolean;
-    likes_count: number;
-  };
+  id: number;
+  content: string;
+  user_id: number;
+  created_at: string;
+  display_name: string;
+  clerk_id: string;
+  is_liked: boolean;
+  likes_count: number;
+};
