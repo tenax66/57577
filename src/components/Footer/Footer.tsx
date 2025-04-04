0ba79743712430bd29d89abb491a8e9fbf420a94
tenax66 <hiyoshir+tenax66@gmail.com>
2025-04-04 23:48:46 +0900

    remove links

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -3,23 +3,11 @@ import styles from './Footer.module.scss';
 
 export const Footer = () => {
   const currentYear = new Date().getFullYear();
-  
+
   return (
     <footer className={styles.footer}>
-      <div className={styles.links}>
-        <Link to="/" className={styles.link}>
-          トップページ
-        </Link>
-        <Link to="/ranking" className={styles.link}>
-          ランキング
-        </Link>
-        <Link to="/search" className={styles.link}>
-          検索
-        </Link>
-      </div>
-      <div className={styles.copyright}>
-        © {currentYear} 57577.net
-      </div>
+      <div className={styles.links}></div>
+      <div className={styles.copyright}>© {currentYear} 57577.net</div>
     </footer>
   );
-}; 
+};
